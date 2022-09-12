0734fd556c239570a30b7733d86d70453d026490
Katya <katvas785@gmail.com>
2022-09-12 19:31:03 +0300

    Ex41

---
+++ Ex41/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите элементы");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int y = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        y++;
+    }
+}
+Console.WriteLine(y);
